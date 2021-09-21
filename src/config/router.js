7b810c3b22bea03a2d65dd4e27d4708916c0adba
pyo-sh <pygosky@gmail.com>
2021-09-21 23:26:07 +0900

    feat: 하단 바를 통해 페이징 이동 구현

--- src/config/router.js
+++ src/config/router.js
@@ -8,6 +8,9 @@ import {
 import Splash from '~/screens/Splash';
 import Login from '~/screens/Login';
 import Main from '~/screens/Main';
+import Search from '~/screens/Search';
+import Recipes from '~/screens/Recipes';
+import Setting from '~/screens/Setting';
 
 const RouterComponent = () => {
     return (
@@ -17,8 +20,16 @@ const RouterComponent = () => {
                     gestureEnabled={false} panHandlers={null} />
                 <Scene key="login" hideNavBar component={Login}
                     gestureEnabled={false} panHandlers={null} />
-                <Scene key="main" hideNavBar component={Main}
-                    gestureEnabled={false} panHandlers={null} />
+                <Scene key="BottomTab" hideNavBar tabs={true} default="main">
+                    <Scene key="main" hideNavBar component={Main}
+                        gestureEnabled={false} panHandlers={null} />
+                    <Scene key="search" hideNavBar component={Search}
+                        gestureEnabled={false} panHandlers={null} />
+                    <Scene key="recipes" hideNavBar component={Recipes}
+                        gestureEnabled={false} panHandlers={null} />
+                    <Scene key="setting" hideNavBar component={Setting}
+                        gestureEnabled={false} panHandlers={null} />
+                </Scene>
             </Stack>
         </Router>
     );
