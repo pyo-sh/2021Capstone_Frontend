58626d51b74944321503a58ec2e360bd7923f580
pyo-sh <pygosky@gmail.com>
2021-09-21 23:26:07 +0900

    feat: 하단 바를 통해 페이징 이동 구현

---
+++ src/screens/Recipes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+    View,
+    Text
+} from 'react-native';
+
+const Recipes = () => {
+    return <View>
+        <Text>
+            Recipes
+        </Text>
+    </View>
+}
+
+export default Recipes;
