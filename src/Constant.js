f7c967aa4a276cc9591b005d49b575cda6c209d7
pyo-sh <pygosky@gmail.com>
2021-09-23 21:36:38 +0900

    feat: 냉장고 페이지 뼈대 완성

--- src/Constant.js
+++ src/Constant.js
@@ -1,6 +1,9 @@
 export const Color = {
-    primary_1: "#4C7885",
-	primary_2: "#b0d8e7",
+    primary_1: "#4c7885",
+	primary_2: "#96b1b8",
+	primary_3: "#acc2c7",
 	white: "#ffffff",
+    background: "#f0f6f5",
     pointed_red: "#e26c7f",
+    pointed_blue: "#b0d8e7",
 }
