8971cb422ddc050c303d90dc538dab9fd6eb0c6d
pyo-sh <pygosky@gmail.com>
2021-09-26 14:51:05 +0900

    feat: Color 조정하기

--- src/Constant.js
+++ src/Constant.js
@@ -1,9 +1,11 @@
 export const Color = {
-    primary_1: "#4c7885",
-	primary_2: "#96b1b8",
-	primary_3: "#acc2c7",
+    primary_1: "#4c7885",   // 글자 색
+	primary_2: "#b0d8e7",   // 아이콘, 버튼
+    primary_3: "#96b1b8",   // 실선
 	white: "#ffffff",
+    black: "#000000",
     background: "#f0f6f5",
     pointed_red: "#e26c7f",
-    pointed_blue: "#b0d8e7",
+    gray: "#707070",
+    
 }
