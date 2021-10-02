2f08c157d6933612fc005180bc82900f679ac12c
pyo-sh <pygosky@gmail.com>
2021-10-03 01:52:03 +0900

    feat: 색 추가 (회색, 진한 메인 색 등)

--- src/Constant.js
+++ src/Constant.js
@@ -2,6 +2,7 @@ export const Color = {
     primary_1: "#4c7885",   // 글자 색
 	primary_2: "#b0d8e7",   // 아이콘, 버튼
     primary_3: "#96b1b8",   // 실선
+    primary_4: "#075063",   // 조금 더 진한 색
 	white: "#ffffff",
     black: "#000000",
     background: "#f0f6f5",
