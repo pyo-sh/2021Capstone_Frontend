2d5857ca4982c1c1ccee05c731974e1b59373e89
pyo-sh <pygosky@gmail.com>
2021-10-03 00:20:36 +0900

    feat: 검색 인풋바 및 돋보기 슬라이드 애니메이션 작성

---
+++ src/components/search/SearchInput.js
@@ -0,0 +1,93 @@
+import React, { useRef, useEffect } from 'react';
+import {
+    StyleSheet,
+    View,
+    Animated,
+    TextInput,
+    TouchableOpacity
+} from 'react-native';
+import { Color } from '~/Constant';
+import SearchIcon from '~/components/icons/SearchIcon';
+
+const SlideRightView = (props) => {
+    const iconX = useRef(new Animated.Value(0)).current;
+    useEffect(() => {
+        Animated.timing(
+            iconX,
+            {
+                toValue: 1,
+                duration: 300,
+                useNativeDriver: true,
+            }
+        ).start();
+    }, [iconX]);
+
+    return (
+        <Animated.View
+            style={[
+                props?.style ?? {},
+                {
+                    transform: [{
+                        translateX: iconX.interpolate({
+                            inputRange: [0, 1],
+                            outputRange: [25, 0]
+                        })
+                    }]
+                },
+            ]}
+        >
+            {props.children}
+        </Animated.View>
+    );
+}
+
+const SearchInput = (props) => {
+    const { text, onChangeText, placeholder, onSubmitEditing } = props;
+
+    return <View style={styleSheet.wrapper}>
+        <TextInput
+            style={styleSheet.input}
+            value={text}
+            onChangeText={onChangeText}
+            placeholder={placeholder}
+            onSubmitEditing={onSubmitEditing}
+        />
+        <View style={styleSheet.interactive}>
+            {(text)
+            ? <SlideRightView style={{ position: 'absolute', zIndex: 3 }}>
+                <TouchableOpacity
+                    onPress={onSubmitEditing}
+                    index={1}
+                >
+                    <SearchIcon/>
+                </TouchableOpacity>
+            </SlideRightView>
+            : <></>
+            }
+        </View>
+    </View>
+}
+
+const styleSheet = StyleSheet.create({
+    wrapper: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    input: {
+        width: '100%',
+        flex: 1,
+        height: 47,
+        paddingLeft: 20,
+        backgroundColor: Color.background,
+        borderRadius: 8,
+    },
+    interactive: {
+        position: 'relative',
+        bottom: 11,
+        right: 40,
+    },
+});
+
+export default SearchInput;
--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+    StyleSheet,
     View,
-    Text
+    TextInput
 } from 'react-native';
+import { Color } from '~/Constant';
+import SearchInput from '~/components/search/SearchInput';
 
 const Search = () => {
+    const [text, setText] = useState('');
+
     return <View>
-        <Text>
-            Search
-        </Text>
+        <View style={styleSheet.topSide}>
+            <SearchInput
+                text={text}
+                onChangeText={(value) => setText(value)}
+                placeholder={"재료 이름을 검색하세요"}
+                onSubmitEditing={() => console.log(text)}
+            />
+        </View>
     </View>
 }
+
+const styleSheet = StyleSheet.create({
+    topSide: {
+        width: '100%',
+        height: 100,
+        paddingVertical: 17,
+        paddingHorizontal: 30,
+
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Color.white,
+    },
+});
 
 export default Search;
