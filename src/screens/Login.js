a0ffc25f43dc3544011d7891a838a6d2c0688b94
jeongname <dlqkqhdi3259@gmail.com>
2021-10-29 22:53:11 +0900

    feat : 회원가입 화면
    회원 가입 화면을 추가했습니다.

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
     StyleSheet,
     View,
@@ -18,25 +18,26 @@ const Login = () => {
         <View style={styleSheet.wrapperalign}>
             <View>
                 <LoginInput
-                    text={textid}
+                    value={textid}
                     onChangeText={(value) => setTextid(value)}
-                    placeholder={'Id'}
+                    placeholder={'ID'}
                     onSubmitEditing={() => console.log(textid)}
                 />
             </View>
             <View>
                 <LoginInput
-                    text={textpw}
+                    value={textpw}
                     onChangeText={(value) => setTextpw(value)}
                     placeholder={'Password'}
                     onSubmitEditing={() => console.log(textpw)}
                 />
             </View>
             <View>
-                <TouchableOpacity style={{
-                    padding : 5,
-                    paddingLeft : 190,
-                }}>
+                <TouchableOpacity onPress={()=>Actions.signup()}
+                    style={{
+                        padding : 5,
+                        paddingLeft : 190,
+                    }}>
                     <Text style={{
                         color : Color.primary_1,
                     }}>
@@ -51,12 +52,12 @@ const Login = () => {
                     style={styleSheet.button}>
                     <SvgXml
                         xml={`
-                            <svg xmlns="http://www.w3.org/2000/svg" width="277" height="55" viewBox="0 0 277 55">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="277" height="55" viewBox="0 0 277 55">
                             <g id="loginButton" transform="translate(-57 -584)">
                                 <path id="패스_7725" data-name="패스 7725" d="M27.5,0h222a27.5,27.5,0,0,1,0,55H27.5a27.5,27.5,0,0,1,0-55Z" transform="translate(57 584)" fill="#b0d8e7"/>
                                 <text id="로그인" transform="translate(164 622)" fill="#075063" font-size="23" font-family="NotoSansCJKkr-Bold, Noto Sans CJK KR" font-weight="700"><tspan x="0" y="0">로그인</tspan></text>
                             </g>
-                            </svg>
+                            </svg>               
                         `}
                     />
                 </TouchableOpacity>
