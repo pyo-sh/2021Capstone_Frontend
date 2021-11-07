fa81f9fc56c8165ef2d2c42f36ff0f5d0cfba4be
pyo-sh <pygosky@gmail.com>
2021-11-08 03:31:10 +0900

    feat : 아이콘 props 전달!

--- src/components/icons/PlusIcon.js
+++ src/components/icons/PlusIcon.js
@@ -2,11 +2,9 @@ import React from "react";
 import { View } from "react-native";
 import { SvgXml } from "react-native-svg";
 
-const icon = (color, width, height) => {
+const icon = (color, width = 30, height = 30) => {
 	return `
-    <svg width="${width ?? 30}" height="${
-		height ?? 35
-	}" viewBox="0 0 42 45" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="${width}" height="${height}" viewBox="0 0 42 45" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M21 1.97705V43.0231V1.97705Z" fill="${color}"/>
         <path d="M21 1.97705V43.0231" stroke="${color}" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M2.05701 22.5H39.942H2.05701Z" fill="${color}"/>
