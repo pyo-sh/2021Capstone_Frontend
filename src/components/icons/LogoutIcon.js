799e0b0046d6d95c95293790db832bc9233322af
pyo-sh <pygosky@gmail.com>
2021-11-17 08:11:30 +0900

    feat: 로그아웃 아이콘 추가

---
+++ src/components/icons/LogoutIcon.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View } from "react-native";
+import { SvgXml } from "react-native-svg";
+
+const icon = color => {
+	return `
+    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="30" height="30" viewBox="0 0 24 24" stroke-width="2" stroke="${color}" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z"/>
+      <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"/>
+      <path d="M7 12h14l-3 -3m0 6l3 -3"/>
+    </svg>
+    `;
+};
+const LogoutIcon = props => {
+	return (
+		<View style={props?.style ?? {}}>
+			<SvgXml xml={icon(props.color)} />
+		</View>
+	);
+};
+
+export default LogoutIcon;
