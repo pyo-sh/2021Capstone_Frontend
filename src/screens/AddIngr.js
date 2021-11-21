5a97ca0122b8bed588669a103369523b4ebb6d12
pyo-sh <pygosky@gmail.com>
2021-11-22 02:00:59 +0900

    refact: 식자재 등록에서 버튼 컴포넌트로 분리

--- src/screens/AddIngr.js
+++ src/screens/AddIngr.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Text, TouchableOpacity, Alert } from "react-native";
 import SelectDropdown from "react-native-select-dropdown";
 import { Actions } from "react-native-router-flux";
+import OtherButtons from "@src/components/add-ingr/OtherButtons";
 import SearchInput from "@src/components/search/SearchInput";
 import CalendarInput from "@src/components/add-ingr/CalendarInput";
 import RadioBtn from "@src/components/add-ingr/RadioBtn";
@@ -51,21 +52,7 @@ const AddIngr = ({ refs, refInfos }) => {
 
 	return (
 		<View style={styleSheet.wrapper}>
-			<View style={styleSheet.otherwise}>
-				<Text style={[styleSheet.title, { fontSize: 16 }]}>식자재 가져오기</Text>
-				<View style={styleSheet.flexLine}>
-					<TouchableOpacity style={styleSheet.otherBtn}>
-						<Text style={styleSheet.otherText}>쇼핑몰</Text>
-						<Text style={styleSheet.otherText}>주문목록</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styleSheet.otherBtn}>
-						<Text style={styleSheet.otherText}>영수증</Text>
-					</TouchableOpacity>
-					<TouchableOpacity style={styleSheet.otherBtn}>
-						<Text style={styleSheet.otherText}>바코드</Text>
-					</TouchableOpacity>
-				</View>
-			</View>
+			<OtherButtons />
 			<View style={styleSheet.controls}>
 				<View style={styleSheet.flexLine}>
 					<Text style={styleSheet.controlTitle}>식자재 명</Text>
@@ -178,42 +165,12 @@ const styleSheet = StyleSheet.create({
 		height: "100%",
 		backgroundColor: Color.background
 	},
-	otherwise: {
-		width: "100%",
-		paddingHorizontal: 20,
-		paddingTop: 20,
-		paddingBottom: 8,
-		marginVertical: 10,
-		backgroundColor: Color.white
-	},
-	title: {
-		marginBottom: 12,
-		...DefaultFont_KR,
-		color: Color.primary_4
-	},
 	flexLine: {
 		marginBottom: 15,
 		display: "flex",
 		flexDirection: "row",
 		justifyContent: "center",
 		alignItems: "center"
-	},
-	otherBtn: {
-		height: 72,
-		padding: 10,
-		margin: 5,
-		flex: 1,
-		display: "flex",
-		flexDirection: "column",
-		justifyContent: "center",
-		alignItems: "center",
-		borderWidth: 1,
-		borderRadius: 10,
-		borderColor: Color.primary_4
-	},
-	otherText: {
-		...DefaultFont_KR,
-		color: Color.primary_1
 	},
 	controls: {
 		width: "100%",
